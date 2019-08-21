f785451c70d51fb9136b51a71e6e464b90470545
5tarlight <developerstarlight@gmail.com>
2019-08-22 00:24:43 +0900

    original tiny headfer

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
 import RightHeaderItem from './RightHeaderItem';
+import LeftHeaderItem from './LeftHeaderItem';
 
 const HeaderWrapper = styled.div`
   width: 100%;
-  height: 3.75rem;
+  height: 1.875rem;
   color: white;
-  background-color: darkblue;
-  line-height: 3.75rem;
+  background-color: #7CC4F8;
+  line-height: 1.675rem;
+  box-shadow: 0px 2px 2px #111111;
+  z-index: 5;
 `
 
 const Header = () => {
   return (
     <HeaderWrapper>
+      <LeftHeaderItem href={'/'}>홈</LeftHeaderItem>
       <RightHeaderItem href={'/login'}>로그인</RightHeaderItem>
     </HeaderWrapper>
   )
