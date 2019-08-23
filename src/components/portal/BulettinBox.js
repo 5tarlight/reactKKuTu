f892c0941437f3a16692767d2f4cbf0af1be23a4
5tarlight <developerstarlight@gmail.com>
2019-08-23 17:51:53 +0900

    bulletin text

--- src/components/portal/BulettinBox.js
+++ src/components/portal/BulettinBox.js
@@ -6,12 +6,15 @@ const Bluettin = styled.div`
   background-color: white;
   height: 35rem;
   overflow: auto;
+  padding: 1rem;
 `
 
 const BulettinBox = () => {
   return (
     <Bluettin>
-      
+      <h1>글자로 놀자! 끄투</h1>
+      <h3>현재 개발이 진행중입니다.</h3>
+      <h4>정상적인 플레이는 불가능 할 수 있습니다.</h4>
     </Bluettin>
   )
 }
